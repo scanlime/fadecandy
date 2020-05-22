4e9c965c2cc753b4a07208ed346fb5e5af45b8e0
Andrew Cole <aocole@gmail.com>
2020-05-21 23:32:00 -0700

    Improve go example: better json & error handling
     * Use more canonical JSON parsing with struct tags
     * Better error handling - no ignored errors
     * Format according to go's canonical `gofmt` command
     * Canonicalize variable names (no underscores)

--- examples/go/scrollserver.go
+++ examples/go/scrollserver.go
@@ -1,128 +1,122 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "io/ioutil"
-    "log"
-    "flag"
-    "time"
-    "math/rand"
-    "encoding/json"
-    "github.com/kellydunn/go-opc"
-)   
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/kellydunn/go-opc"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+)
 
 type Color struct {
-    R, G, B uint8
+	R uint8 `json:"r"`
+	G uint8 `json:"g"`
+	B uint8 `json:"b"`
 }
 
 type Scroller struct {
-    delay, train_len int
-    random bool
-    color Color
+	Delay    int   `json:"delay"`
+	TrainLen int   `json:"train_len"`
+	Random   bool  `json:"random"`
+	Color    Color `json:"color"`
 }
 
-var color = Color{255,0,0}
-var home_c chan Scroller
+var homeChan chan Scroller
 
-func random(min, max int) uint8 {
-    xr := rand.Intn(max - min) + min
-    return uint8(xr)
+func Random(min, max int) uint8 {
+	xr := rand.Intn(max-min) + min
+	return uint8(xr)
 }
 
 func main() {
-    rand.Seed(time.Now().Unix())
-    
-    serverPtr := flag.String("fcserver", "localhost:7890", "Fadecandy server and port to connect to")
-    listenPortPtr := flag.Int("port", 8080, "Port to serve UI from")
-    leds_len := flag.Int("leds", 40, "Number of LEDs in the string")
-    flag.Parse()
-    
-    home_c = make(chan Scroller, 1)
+	rand.Seed(time.Now().Unix())
 
-    go func() { LEDSender(home_c, *serverPtr, *leds_len) }()
-    
-    fs := http.FileServer(http.Dir("./static"))
-    http.Handle("/static/", http.StripPrefix("/static/", fs))
-    http.Handle("/", http.StripPrefix("/", fs))
-    http.HandleFunc("/update", UpdateHandler)
+	serverPtr := flag.String("fcserver", "localhost:7890", "Fadecandy server and port to connect to")
+	listenPortPtr := flag.Int("port", 8080, "Port to serve UI from")
+	ledsLen := flag.Int("leds", 64, "Number of LEDs in the string")
+	flag.Parse()
 
-    log.Println("Listening on",fmt.Sprintf("http://0.0.0.0:%d",*listenPortPtr), "...")
-    http.ListenAndServe(fmt.Sprintf(":%d",*listenPortPtr), nil)
-}   
+	homeChan = make(chan Scroller, 1)
+
+	go func() { LEDSender(homeChan, *serverPtr, *ledsLen) }()
+
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.Handle("/", http.StripPrefix("/", fs))
+	http.HandleFunc("/update", UpdateHandler)
+
+	log.Println("Listening on", fmt.Sprintf("http://0.0.0.0:%d", *listenPortPtr), "...")
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *listenPortPtr), nil))
+}
 
 func UpdateHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal("Could not read request body", err)
+	}
+	var inscroll Scroller
+	err = json.Unmarshal(body, &inscroll)
+	if err != nil {
+		log.Fatal("Could not unmarshal request body", err)
+	}
 
-    // do these stupid hacks for parsing JSON. 
-    // go is pretty bad at this
-    body, _ := ioutil.ReadAll(r.Body)    
-    var f interface{}
-    var inscroll Scroller
-    json.Unmarshal(body, &f)
+	ss := inscroll
 
-    m := f.(map[string]interface{})
+	//send on the home channel, nonblocking
+	select {
+	case homeChan <- ss:
+	default:
+		log.Println("msg NOT sent")
+	}
 
-    inscroll.delay = int(m["delay"].(float64))
-    inscroll.train_len = int(m["train_len"].(float64))
-    inscroll.random = bool(m["random"].(bool))
-    colormap := m["color"].(map[string]interface{})
-    inscroll.color.R = uint8(colormap["r"].(float64))
-    inscroll.color.G = uint8(colormap["g"].(float64))
-    inscroll.color.B = uint8(colormap["b"].(float64))
+	fmt.Fprintf(w, "HomeHandler %d", ss.Delay)
+}
 
-    ss := inscroll
-    
-    //send on the home channel, nonblocking
-    select {
-        case home_c <- ss:
-        default:
-            log.Println("msg NOT sent")
-    }
+func LEDSender(c chan Scroller, server string, ledsLen int) {
 
-    fmt.Fprintf(w, "HomeHandler", ss.delay)
-} 
+	props := Scroller{ledsLen, 7, false, Color{255, 0, 0}}
+	props.Delay = 100
 
-func LEDSender( c chan Scroller, server string, leds_len int ) {
-        
-    props := Scroller{40, 7, false, Color{255,0,0}}
-    props.delay = 100
+	// Create a client
+	oc := opc.NewClient()
+	err := oc.Connect("tcp", server)
+	if err != nil {
+		log.Fatal("Could not connect to Fadecandy server", err)
+	}
 
-     // Create a client
-    oc := opc.NewClient()
-    err := oc.Connect("tcp", server)
-    if err != nil {
-        log.Fatal("Could not connect to Fadecandy server", err)
-    }
+	for {
+		for i := 0; i < ledsLen; i++ {
+			// send pixel data
+			m := opc.NewMessage(0)
+			m.SetLength(uint16(ledsLen * 3))
 
-    for { 
-        for i := 0; i < leds_len; i++ {
-            // send pixel data
-            m := opc.NewMessage(0)
-            m.SetLength(uint16(leds_len*3))
-            
-            for ii := 0; ii < props.train_len; ii++ {
-                pix := i+ii
-                if pix >= leds_len {
-                    pix = props.train_len - ii - 1
-                }
-                if props.random {
-                    m.SetPixelColor(pix, random(2,255), random(2,255), random(2,255))
-                } else {
-                    m.SetPixelColor(pix, props.color.R, props.color.G, props.color.B)
-                }
-            }
-            
-            err := oc.Send(m) 
-            if err != nil {
-                log.Println("couldn't send color",err)
-            }             
-            time.Sleep(time.Duration(props.delay) * time.Millisecond)
+			for ii := 0; ii < props.TrainLen; ii++ {
+				pix := i + ii
+				if pix >= ledsLen {
+					pix = props.TrainLen - ii - 1
+				}
+				if props.Random {
+					m.SetPixelColor(pix, Random(2, 255), Random(2, 255), Random(2, 255))
+				} else {
+					m.SetPixelColor(pix, props.Color.R, props.Color.G, props.Color.B)
+				}
+			}
 
-            // receive from channel
-            select {
-                case props = <- c:
-                default:
-            }
-        }
-    }
+			err := oc.Send(m)
+			if err != nil {
+				log.Println("couldn't send Color", err)
+			}
+			time.Sleep(time.Duration(props.Delay) * time.Millisecond)
+
+			// receive from channel
+			select {
+			case props = <-c:
+			default:
+			}
+		}
+	}
 }
